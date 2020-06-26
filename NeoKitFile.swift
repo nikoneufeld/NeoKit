e3c7a15921b2640359d578c19f2e62f496eca640
Niko Neufeld <niko.neufeld@gmail.com>
2020-06-26 11:37:30 +0200

    Create NeoKitFile.swift

---
+++ NeoKitFile.swift
@@ -0,0 +1,190 @@
+// All Credits to Niko Neufeld and Paul Hudson for the shadows and overlays.
+import SwiftUI
+extension Color {
+    static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+    static let darkStart = Color(red: 50 / 255, green: 60 / 255, blue: 65 / 255)
+    static let darkEnd = Color(red: 25 / 255, green: 25 / 255, blue: 30 / 255)
+    static let lightStart = Color(red: 60 / 255, green: 160 / 255, blue: 240 / 255)
+    static let lightEnd = Color(red: 30 / 255, green: 80 / 255, blue: 120 / 255)
+    
+}
+struct DarkToggleStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }) {
+            configuration.label
+                .padding(30)
+                .contentShape(Circle())
+        }
+        .background(
+            DarkBackground(isHighlighted: configuration.isOn, shape: Circle())
+        )
+    }
+}
+let dark_grad = LinearGradient(Color.darkStart, Color.darkEnd)
+struct DarkBackground<S: Shape>: View {
+    var isHighlighted: Bool
+    var shape: S
+
+    var body: some View {
+        ZStack {
+            if isHighlighted {
+                shape
+                    .fill(LinearGradient(Color.darkEnd, Color.darkStart))
+                    .overlay(shape.stroke(LinearGradient(Color.darkStart, Color.darkEnd), lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
+                    .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
+
+            } else {
+                shape
+                    .fill(LinearGradient(Color.darkStart, Color.darkEnd))
+                    .overlay(shape.stroke(Color.darkEnd, lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
+                    .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
+            }
+        }
+    }
+}
+struct ColorfulBackground<S: Shape>: View {
+    var isHighlighted: Bool
+    var shape: S
+
+    var body: some View {
+        ZStack {
+            if isHighlighted {
+                shape
+                        .fill(LinearGradient(Color.lightEnd, Color.lightStart))
+                        .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                        .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
+                        .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
+            } else {
+                shape
+                            .fill(LinearGradient(Color.darkStart, Color.darkEnd))
+                            .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                            .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
+                            .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
+                    }
+            }
+        }
+    }
+struct ColorfulButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                ColorfulBackground(isHighlighted: configuration.isPressed, shape: Circle())
+            )
+            .animation(nil)
+    }
+}
+
+struct ColorfulToggleStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }) {
+            configuration.label
+                .padding(30)
+                .contentShape(Circle())
+        }
+        .background(
+            ColorfulBackground(isHighlighted: configuration.isOn, shape: Circle())
+        )
+    }
+}
+
+struct DarkButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                DarkBackground(isHighlighted: configuration.isPressed, shape: Circle())
+            )
+    }
+}
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
+struct NeoInButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+//        configuration.label
+//            .padding(30)
+//            .background(
+//                Circle()
+//                    .fill(Color.offWhite)
+//                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+//                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+//            )
+        configuration.label
+            .padding(30)
+            .background(
+                Group {
+                    if configuration.isPressed {
+                        Circle()
+                            .fill(Color.offWhite)
+                    } else {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y: 2)
+                                    .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                            )
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 8)
+                                    .blur(radius: 4)
+                                    .offset(x: -2, y: -2)
+                                    .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                            )
+                    }
+                }
+            )
+    }
+}
+struct NeoOutButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+//        configuration.label
+//            .padding(30)
+//            .background(
+//                Circle()
+//                    .fill(Color.offWhite)
+//                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+//                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+//            )
+        configuration.label
+            .padding(30)
+            .background(
+                Group {
+                    if !configuration.isPressed {
+                        Circle()
+                            .fill(Color.offWhite)
+                    } else {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y: 2)
+                                    .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                            )
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 8)
+                                    .blur(radius: 4)
+                                    .offset(x: -2, y: -2)
+                                    .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                            )
+                    }
+                }
+            )
+    }
+}
